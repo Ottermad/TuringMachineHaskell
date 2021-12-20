a222928a9f0d3da884b14486c4434c00e331265e
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2021-12-20 14:42:40 +0000

    Use new helper in main

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,10 @@
-module Main where
+    module Main where
 
 import Turing
 
 main :: IO ()
 main = do
-    let myMachine = TuringMachine{
-        states = [Halt, StartState, A, B, C],
-        currentState = StartState,
-        tape = [Start, One, Zero, One, Blank], -- Can I use an infinite list of blanks here?
-        currentPosition = 0,
-        instructions = [
+    let myMachine = newTuringMachine [One, Zero, One] [
             Instruction{stateToMatch = StartState, symbolToMatch=Start, newState = A, newSymbol = Start, positionShift = Forwards},
             Instruction{stateToMatch = A, symbolToMatch=Zero, newState = A, newSymbol = Blank, positionShift = Forwards},
             Instruction{stateToMatch = A, symbolToMatch=One, newState = A, newSymbol = Blank, positionShift = Forwards},
@@ -17,9 +12,8 @@ main = do
             Instruction{stateToMatch = B, symbolToMatch=Blank, newState = B, newSymbol = Blank, positionShift = Backwards},
             Instruction{stateToMatch = B, symbolToMatch=Start, newState = C, newSymbol = Start, positionShift = Forwards},
             Instruction{stateToMatch = C, symbolToMatch=Blank, newState = Halt, newSymbol = One, positionShift = Stay}
-        ]
-    }
-    print (tape myMachine)
+            ]
+    print (take 10 (tape myMachine))
     let outputMachine = runMachine myMachine
-    print (tape outputMachine)
+    print (take 10 (tape outputMachine))
 
